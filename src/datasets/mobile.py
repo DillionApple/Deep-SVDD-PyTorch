fe9f5a23157bffd8b279ab8f86c97c28182f5746
DillionApple <dillionmango@gmail.com>
2019-05-02 15:24:34 +0800

    add mobile_LeNet

---
+++ src/datasets/mobile.py
@@ -0,0 +1,97 @@
+import os
+
+from PIL import Image
+from torch.utils.data.dataset import Dataset
+from base.torchvision_dataset import TorchvisionDataset
+from .preprocessing import global_contrast_normalization
+
+import torchvision.transforms as transforms
+
+
+class Mobile_Dataset(TorchvisionDataset):
+
+    def __init__(self, root: str, normal_class=0):
+        super().__init__(root)
+
+        self.n_classes = 2  # 0: normal, 1: outlier
+        self.normal_classes = tuple([0])
+        self.outlier_classes = tuple([1])
+
+        # Pre-computed min and max values (after applying GCN) from train data per class
+        min_max = [(-28.94083453598571, 13.802961825439636),
+                   (-6.681770233365245, 9.158067708230273),
+                   (-34.924463588638204, 14.419298165027628),
+                   (-10.599172931391799, 11.093187820377565),
+                   (-11.945022995801637, 10.628045447867583),
+                   (-9.691969487694928, 8.948326776180823),
+                   (-9.174940012342555, 13.847014686472365),
+                   (-6.876682005899029, 12.282371383343161),
+                   (-15.603507135507172, 15.2464923804279),
+                   (-6.132882973622672, 8.046098172351265)]
+
+        # CIFAR-10 preprocessing: GCN (with L1 norm) and min-max feature scaling to [0,1]
+        transform = transforms.Compose([transforms.ToTensor(),
+                                        transforms.Lambda(lambda x: global_contrast_normalization(x, scale='l1')),
+                                        transforms.Normalize([min_max[normal_class][0]] * 3,
+                                                             [min_max[normal_class][1] - min_max[normal_class][0]] * 3)])
+
+        self.train_set = Mobile(
+            os.path.join(self.root, "training"),
+            os.path.join(self.root, "test"),
+            transform,
+            True
+        )
+
+        self.test_set = Mobile(
+            os.path.join(self.root, "training"),
+            os.path.join(self.root, "test"),
+            transform,
+            False
+        )
+
+
+class Mobile(Dataset):
+    """Torchvision CIFAR10 class with patch of __getitem__ method to also return the index of a data sample."""
+
+    def __load_images_from_path(self, root):
+        ret = []
+        for file in os.listdir(root):
+            filename = os.fsdecode(file)
+            filepath = os.path.join(root, filename)
+            img = Image.open(filepath).resize((256, 256)).convert("L")
+            ret.append(img)
+        return ret
+
+    def __init__(self, train_root, test_root, transform, train):
+        self.transform = transform
+
+        self.train_data = self.__load_images_from_path(train_root)
+        self.train_labels = [0 for i in range(len(self.train_data))]
+
+        self.test_data = self.__load_images_from_path(test_root)
+        self.test_labels = [1 for i in range(len(self.test_data))]
+
+        self.train = train
+
+    def __getitem__(self, index):
+        """Override the original method of the CIFAR10 class.
+        Args:
+            index (int): Index
+        Returns:
+            triple: (image, target, index) where target is index of the target class.
+        """
+        if self.train:
+            img, target = self.train_data[index], self.train_labels[index]
+        else:
+            img, target = self.test_data[index], self.test_labels[index]
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        return img, target, index  # only line changed
+
+    def __len__(self):
+        if self.train:
+            return len(self.train_data)
+        else:
+            return len(self.test_data)
--- src/networks/main.py
+++ src/networks/main.py
@@ -1,11 +1,11 @@
 from .mnist_LeNet import MNIST_LeNet, MNIST_LeNet_Autoencoder
 from .cifar10_LeNet import CIFAR10_LeNet, CIFAR10_LeNet_Autoencoder
-
+from .mobile_LeNet import Mobile_LeNet, Mobile_LeNet_Autoencoder
 
 def build_network(net_name):
     """Builds the neural network."""
 
-    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet')
+    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'mobile_LeNet')
     assert net_name in implemented_networks
 
     net = None
@@ -16,13 +16,16 @@ def build_network(net_name):
     if net_name == 'cifar10_LeNet':
         net = CIFAR10_LeNet()
 
+    if net_name == 'mobile_LeNet':
+        net = Mobile_LeNet()
+
     return net
 
 
 def build_autoencoder(net_name):
     """Builds the corresponding autoencoder network."""
 
-    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet')
+    implemented_networks = ('mnist_LeNet', 'cifar10_LeNet', 'mobile_LeNet')
     assert net_name in implemented_networks
 
     ae_net = None
@@ -32,5 +35,8 @@ def build_autoencoder(net_name):
 
     if net_name == 'cifar10_LeNet':
         ae_net = CIFAR10_LeNet_Autoencoder()
+
+    if net_name == 'mobile_LeNet':
+        ae_net = Mobile_LeNet_Autoencoder()
 
     return ae_net
